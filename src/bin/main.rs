b830d3eae58e884e8156de2b70f0d6b19c86133c
LXK <lxk@droidcraft.org>
2017-10-10 18:10:01 +0300

    not yet working with compiler

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,15 +2,20 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs::File;
+use std::thread;
+use std::time::Duration;
 
+extern crate rust_status;
+use rust_status::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {handle_connection(stream);});
     }
 }
 
@@ -19,8 +24,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "src/default.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "src/default.html")
     } else {
         ("HTTP/1.1 404 ZERO FUCKS GIVEN\r\n\r\n", "src/404.html")
